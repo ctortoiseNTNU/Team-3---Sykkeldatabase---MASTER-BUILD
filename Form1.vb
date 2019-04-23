739fc0616e87408c59e7031759d27ac73c52596e
ivilivis <44821879+ivilivis@users.noreply.github.com>
2019-04-23 22:43:17 +0200

    testing

--- Form1.vb
+++ Form1.vb
@@ -6,6 +6,7 @@ Public Class Form1
     'som gjenbrukes over programmet. De er uten navnekonvensjon
     'Husk å kommentere på kodensfunksjon og hvor i programmet den er tatt i bruk.
     'Test forandring
+    ' Test hilsen IVar
     'Globalevariabler som må sjekkes ved ønsket tilgang. LogBool settes til TRUE ved inlogging, AdminBool settes til TRUE hvis bruker er admin.
     Dim LogBool As Boolean
     Dim AdminBool As Boolean
